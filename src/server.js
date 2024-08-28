2fe04b421832c200b677f657317b48507f52b092
endziu <koper.andrzej@gmail.com>
2024-08-28 17:30:32 +0200

    save

--- src/server.js
+++ src/server.js
@@ -12,8 +12,9 @@ app.use(errorHandler);
 
 ensureDirectoriesExist()
   .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
+    const port = process.env.PORT || PORT;
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
     });
   })
   .catch((error) => {
