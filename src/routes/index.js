83f2655d21bd587c06ec50454b81d58943b2215b
endziu <koper.andrzej@gmail.com>
2024-08-28 19:47:05 +0200

    little refactoring

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,33 +2,10 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs').promises;
 const { OUTPUT_DIR } = require('../config');
-const { generateRandomFileName, cleanupFiles, executeCommand } = require('../utils');
+const { generateRandomFileName, cleanupFiles, generateWaveform, generateSpectrogram } = require('../utils');
 const { createUploadMiddleware } = require('../middleware');
 
-const generateWaveform = async (inputFile, outputFile) => {
-  const command = `
-    ffmpeg -i ${inputFile} -filter_complex "
-      aformat=channel_layouts=mono,
-      compand=attacks=0:points=-80/-900|-45/-15|-27/-9|-5/-5|0/-2|20/-2:gain=5,
-      showwavespic=s=1920x1080:colors=#333333
-    " -frames:v 1 ${outputFile}
-  `;
-  await executeCommand(command);
-};
-
-const generateSpectrogram = async (inputFile, outputFile) => {
-  const command = `
-    ffmpeg -i ${inputFile} -lavfi "
-      aformat=channel_layouts=mono,
-      compand=attacks=0:points=-80/-900|-45/-15|-27/-9|-5/-5|0/-2|20/-2:gain=5,
-      showspectrumpic=s=1920x1080:mode=separate:color=intensity:scale=log:
-      fscale=log:stop=20000:start=20:gain=5:legend=0
-    " -frames:v 1 ${outputFile}
-  `;
-  await executeCommand(command);
-};
-
-const handleGenerateVisualization = (generateFunction) => async (req, res) => {
+const handleVisualization = (generateFunction) => async (req, res) => {
   if (!req.file) {
     return res.status(400).json({ error: 'No audio file uploaded' });
   }
@@ -57,8 +34,8 @@ const setupRoutes = (app) => {
     res.sendFile(path.join(process.cwd(), 'public', 'index.html'));
   });
 
-  app.post('/generate-waveform', createUploadMiddleware(), handleGenerateVisualization(generateWaveform));
-  app.post('/generate-spectrogram', createUploadMiddleware(), handleGenerateVisualization(generateSpectrogram));
+  app.post('/generate-waveform', createUploadMiddleware(), handleVisualization(generateWaveform));
+  app.post('/generate-spectrogram', createUploadMiddleware(), handleVisualization(generateSpectrogram));
 };
 
 module.exports = { setupRoutes };
--- src/utils/index.js
+++ src/utils/index.js
@@ -3,29 +3,15 @@ const crypto = require('crypto');
 const { exec } = require('child_process');
 const { UPLOAD_DIR, OUTPUT_DIR } = require('../config');
 
-exports.ensureDirectoriesExist = async () => {
-  await fs.mkdir(UPLOAD_DIR, { recursive: true });
-  await fs.mkdir(OUTPUT_DIR, { recursive: true });
-};
-
 exports.generateRandomFileName = () => `${crypto.randomBytes(16).toString('hex')}.png`;
-
-exports.executeCommand = (command) => new Promise((resolve, reject) => {
-  exec(command, (error, stdout, stderr) => {
-    if (error) {
-      console.error('Command execution error:', error);
-      console.error('stderr:', stderr);
-      reject(error);
-    } else {
-      console.log('Command executed successfully');
-      console.log('stdout:', stdout);
-      resolve();
-    }
-  });
-});
-
+exports.ensureDirectoriesExist = async () => {
+  await Promise.all([
+    fs.mkdir(UPLOAD_DIR, { recursive: true }),
+    fs.mkdir(OUTPUT_DIR, { recursive: true })
+  ]);
+};
 exports.cleanupFiles = async (...files) => {
-  for (const file of files) {
+  await Promise.all(files.map(async (file) => {
     try {
       await fs.access(file);
       await fs.unlink(file);
@@ -33,5 +19,28 @@ exports.cleanupFiles = async (...files) => {
     } catch (error) {
       console.error(`Error deleting file ${file}:`, error.message);
     }
-  }
+  }));
 };
+
+const waveformParams = '-filter_complex "aformat=channel_layouts=mono,compand=attacks=0:points=-80/-900|-45/-15|-27/-9|-5/-5|0/-2|20/-2:gain=5,showwavespic=s=1920x1080:colors=#333333"'
+const spectralParams = '-lavf "aformat=channel_layouts=mono,compand=attacks=0:points=-80/-900|-45/-15|-27/-9|-5/-5|0/-2|20/-2:gain=5,showspectrumpic=s=1920x1080:mode=separate:color=intensity:scale=log:fscale=log:stop=20000:start=20:gain=5:legend=0"'
+const generateVisualization = (inputFile, outputFile, params) => {
+  return new Promise((resolve, reject) => {
+    const command = `ffmpeg -i ${inputFile} ${params} -frames:v 1 ${outputFile}`;
+
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error('Visualization generation error:', error);
+        console.error('stderr:', stderr);
+        reject(error);
+      } else {
+        console.log('Visualization generated successfully');
+        console.log('stdout:', stdout);
+        resolve();
+      }
+    });
+  });
+};
+
+exports.generateWaveform = (inputFile, outputFile) => generateVisualization(inputFile, outputFile, waveformParams);
+exports.generateSpectrogram = (inputFile, outputFile) => generateVisualization(inputFile, outputFile, spectralParams);
