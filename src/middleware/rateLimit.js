9afdb89877a7628daf9d0db17ad0e67a9248af29
ladydao <dao.nomad@proton.me>
2024-09-01 11:56:46 +0200

    set rate limit

--- src/middleware/rateLimit.js
+++ src/middleware/rateLimit.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 exports.limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 10, // Limit each IP to 10 requests per windowMs
+  max: 100, // Limit each IP to 100 requests per windowMs
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   message: 'Too many requests from this IP, please try again later.',
